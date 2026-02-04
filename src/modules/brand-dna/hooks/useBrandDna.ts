277f1f97c6096610bc73775beda938094151bdd6
Harrison Zuleta Montoya <harrison.zmontoya@gmail.com>
2026-02-04 12:03:23 -0500

    refactor: simplify useBrandDna to wrap useBrands
    - Make useBrandDna a thin wrapper around core useBrands hook
    - Reuse existing brand selection logic from brandSlice
    - Maintain backward compatibility for Brand DNA module

--- src/modules/brand-dna/hooks/useBrandDna.ts
+++ src/modules/brand-dna/hooks/useBrandDna.ts
@@ -1,45 +1,29 @@
 // useBrandDna.ts
 //
-// This file defines a custom React hook for accessing Brand DNA data from Redux.
-// It provides convenient access to the Brand DNA state and dispatch actions.
+// This file defines a custom React hook for accessing Brand DNA data.
+// It's a wrapper around useBrands for backward compatibility with the Brand DNA module.
 
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/core/store/rootReducer';
-import { AppDispatch } from '@/core/store/store';
-import { fetchBrandDna, clearBrandDna, setSelectedCompany } from '../store/brandDnaSlice';
-import { Company } from '../types/brandDnaTypes';
+import { useBrands } from "@/core/hooks/useBrands";
+import { Brand } from "@/core/schemas/brand-schema";
 
 /**
  * useBrandDna
  *
  * Custom hook that provides access to Brand DNA state and actions.
- * Fetches Brand DNA data when a company is selected.
- *
- * @returns {BrandDnaState & { refetch: (url?: string) => void, clear: () => void, selectCompany: (company: Company) => void }} - Brand DNA state and utility functions
+ * Uses the core useBrands hook for brand selection and data.
  */
 export const useBrandDna = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const brandDna = useSelector((state: RootState) => state.brandDna);
-
-  const selectCompany = (company: Company) => {
-    dispatch(setSelectedCompany(company));
-    dispatch(fetchBrandDna(company.url));
-  };
-
-  const refetch = (url?: string) => {
-    const urlToUse = url || brandDna.selectedCompany?.url;
-    dispatch(fetchBrandDna(urlToUse));
-  };
-
-  const clear = () => {
-    dispatch(clearBrandDna());
-  };
+  const { selectedBrand, selectBrand, loading, brands } = useBrands();
 
   return {
-    ...brandDna,
-    refetch,
-    clear,
-    selectCompany,
+    selectedCompany: selectedBrand,
+    selectCompany: (brand: Brand) => {
+      console.log("useBrandDna - selectCompany called with:", brand);
+      console.log("useBrandDna - brand.uuid:", brand.uuid);
+      selectBrand(brand.uuid);
+    },
+    data: selectedBrand,
+    loading,
+    brands,
   };
 };
-
